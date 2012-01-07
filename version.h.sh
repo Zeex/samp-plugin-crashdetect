597abb36f109bd5ef97820f000b15ef7996882e8
Zeex <zeex@rocketmail.com>
2012-01-07 19:20:30 +0600

    Improve version scripts
    This allows one to use versions of the form vX.Y.

--- version.h.sh
+++ version.h.sh
@@ -1,11 +1,13 @@
 me=`basename $`
 
 # Try git describe first, then VERSION.txt
-version=`command -v git >/dev/null && git describe --match v[0-9]*.[0-9]*.[0-9]* 2>/dev/null\
-			| sed -e 's/-/./g' -e 's/v//' -e 's/\.g.*//' 2>/dev/null\
+version=`command -v git >/dev/null && git describe --match v[0-9]*.[0-9]** 2>/dev/null\
+			| sed -e 's/v//' -e 's/\-g.*//' 2>/dev/null\
 		|| cat VERSION.txt | sed 's/^[ \t]*//;s/[ \t]*$//'\
 		|| echo "$me: Failed to get version info" && exit 1`
-version_rc=`echo $version | sed -e 's/\./,/g' -e 's/^\([0-9]*,[0-9]*,[0-9]*\)$/\1,0/'`
+version_rc=`echo $version | sed -e 's/[-\.]/,/g'\
+                                -e 's/^\([0-9]*,[0-9]*\)$/\1,0,0/'\
+                                -e 's/^\([0-9]*,[0-9]*,[0-9]*\)$/\1,0/'`
 
 echo \#define CRASHDETECT_VERSION \"$version\"
 echo \#define CRASHDETECT_VERSION_RC $version_rc
