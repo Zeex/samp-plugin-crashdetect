9de4856cb99efdb728909b231b33c6ecf6fc8f25
Zeex <zeex@rocketmail.com>
2012-01-14 01:42:04 +0600

    Delete stupid comment

--- src/amxerror.cpp
+++ src/amxerror.cpp
@@ -1,7 +1,6 @@
 #include "crashdetect.h"
 #include "amx/amx.h"
 
-/// Gets called before amx_Exec() on error but before AMX stack/heap is reset
 extern "C" int AMXAPI amx_Error(AMX *amx, cell index, int error) {
 	if (error != AMX_ERR_NONE) {
 		crashdetect::RuntimeError(amx, index, error);		
