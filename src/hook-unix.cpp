d49c4204228bca6692323239505f64bf9214f243
Zeex <zeex@rocketmail.com>
2013-01-06 20:59:56 +0700

    Refactor *nix version Hook::Unprotect()

--- src/hook-unix.cpp
+++ src/hook-unix.cpp
@@ -22,16 +22,15 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdint.h>
+#include <cstddef>
+
 #include <unistd.h>
 #include <sys/mman.h>
 
 #include "hook.h"
 
 void Hook::Unprotect(void *address, int size) {
-	// Both address and size must be multiples of page size...
-	size_t pagesize = getpagesize();
-	size_t where = ((reinterpret_cast<uint32_t>(address) / pagesize) * pagesize);
-	size_t count = (size / pagesize) * pagesize + pagesize * 2;
-	mprotect(reinterpret_cast<void*>(where), count, PROT_READ | PROT_WRITE | PROT_EXEC);
+	std::ptrdiff_t aligned_address = reinterpret_cast<std::ptrdiff_t>(address);
+	aligned_address &= ~(getpagesize() - 1);
+	mprotect(reinterpret_cast<void*>(aligned_address), size, PROT_READ | PROT_WRITE | PROT_EXEC);
 }
