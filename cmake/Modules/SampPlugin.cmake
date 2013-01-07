86d78f7bf97c11ffb3daa3b8b3d89bc3a114db57
Zeex <zeex@rocketmail.com>
2013-01-08 02:41:38 +0700

    Introduce the SampPlugin module

---
+++ cmake/Modules/SampPlugin.cmake
@@ -0,0 +1,28 @@
+# SampPlugin.cmake
+#
+# Functions:
+#   add_samp_plugin(name [sources...])
+#
+# Supported platforms:
+#   Windows
+#   Linux
+#
+# Supported compilers:
+#   GCC
+#   MinGW
+#   Microsoft Visual C++
+
+function(add_samp_plugin name)
+	add_library(${name} MODULE ${ARGN})
+
+	set_target_properties(${name} PROPERTIES PREFIX "")
+
+	if(CMAKE_COMPILER_IS_GNUCC)
+		set_property(TARGET ${name} APPEND PROPERTY COMPILE_FLAGS "-m32")
+		set_property(TARGET ${name} APPEND PROPERTY LINK_FLAGS    "-m32")
+	endif()
+
+	if(UNIX AND NOT WIN32)
+		set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "LINUX")
+	endif()
+endfunction()
