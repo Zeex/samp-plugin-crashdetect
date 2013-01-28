879edfe99d27a9b908b107c164ab99322389a4b4
Zeex <zeex@rocketmail.com>
2013-01-29 06:08:56 +0700

    Fix compile warning about redefinition of alloca

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -22,6 +22,9 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#ifdef _WIN32
+	#include <malloc.h>
+#endif
 #include <amx/amx.h>
 
 #include "amx.h"
