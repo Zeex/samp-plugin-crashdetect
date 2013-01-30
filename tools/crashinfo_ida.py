e5ab3c8f05f2137ff0b8a07677d862019ee51af4
Zeex <zeex@rocketmail.com>
2013-01-30 13:39:32 +0700

    Add IDAPython wrapper around crashinfo.py
    This script can be launched from IDA using IDAPython plugin.
    It should produce more accurate call stack than crashinfo.py
    and also outputs function names

---
+++ tools/crashinfo_ida.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 Zeex
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import crashinfo
+import idc
+import sys
+
+def main(argv):
+  filename = idc.AskFile(forsave=False, mask='crashinfo.txt|*.*', prompt='Choose your crashinfo.txt')
+  if filename is not None:
+    with open(filename, 'r') as file:
+      info = crashinfo.parse_crashinfo(file)
+      print('Call stack:')
+      for address, module in info.get_call_stack():
+        name = idc.GetFunctionName(address)
+        if name:
+          print('  %x in %s!%s' % (address, module.filename, name))
+
+if __name__ == '__main__':
+  main(sys.argv)
