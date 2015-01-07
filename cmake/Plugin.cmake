a563d76d6d5329e9694f12d48486559094bfff0e
Zeex <zeex@rocketmail.com>
2015-01-07 08:51:30 +0600

    Add Clang support

--- cmake/Plugin.cmake
+++ cmake/Plugin.cmake
@@ -1,11 +1,21 @@
+cmake_minimum_required(VERSION 2.8.6)
+
 function(add_plugin name)
   add_library(${name} MODULE ${ARGN})
 
   set_target_properties(${name} PROPERTIES PREFIX "")
 
-  if(CMAKE_COMPILER_IS_GNUCC)
+  if(CMAKE_COMPILER_IS_GNUCC OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     set_property(TARGET ${name} APPEND_STRING PROPERTY COMPILE_FLAGS " -m32")
     set_property(TARGET ${name} APPEND_STRING PROPERTY LINK_FLAGS    " -m32")
+  endif()
+
+  if(CMAKE_COMPILER_IS_GNUCXX)
+    target_compile_options(${name} PRIVATE -Wno-attributes)
+  endif()
+
+  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    target_compile_options(${name} PRIVATE -Wno-ignored-attributes)
   endif()
 
   if(WIN32 AND CMAKE_COMPILER_IS_GNUCC)
